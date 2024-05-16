5555ec4b51fd321216557128af279b0eb958e229
beluga-T <tswlovexyb@outlook.com>
2024-05-15 20:03:28 -0700

    chore: Refactor delete functionality to remove post from posts array

--- server.js
+++ server.js
@@ -231,7 +231,7 @@ app.get('/logout', (req, res) => {
 });
 app.post('/delete/:id', isAuthenticated, (req, res) => {
     // TODO: Delete a post if the current user is the owner
-    const postId = parseInt(req.params.id, 10);
+    const postId = parseInt(req.params.id, 10); 
     posts = posts.filter(p => p.id !== postId || p.username !== req.session.userId);
     res.redirect('/');
 });
